3e30ee41cf31f21d184a0f73d8016b11717d3803
moist-webDev <hope3123@gmail.com>
2023-06-06 00:17:25 -0400

    easier ui handling

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,22 +1,27 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity} from 'react-native';
 
 export default function Button({ title='', theme={}, onPress, style={} }:any){
     return (
-        <Text onPress={onPress} style={{...cstyle(theme).button, ...style}}>{title}</Text>
+        <TouchableOpacity onPress={onPress} style={{...cstyle(theme).button, ...style}}>
+            <Text style={cstyle(theme).text}>{title}</Text>
+        </TouchableOpacity>
     )
 }
 
 const cstyle = (theme:any) => {
     return StyleSheet.create({
-        container: {
-        },
         button: {
             backgroundColor: theme.primary,
             padding: 10,
             alignContent: 'center',
+            textAlign: 'center',
+            justifyContent: 'center',
             color: theme.text,
             margin: 10,
             borderRadius: 5,
+        },
+        text: {
+            color: theme.text,
         },
     });
 }
